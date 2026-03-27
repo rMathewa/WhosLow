b6671cef19a3549ef0f411a9d8b3da836faa6d48
Mathewwa <anxi50408@gmail.com>
2026-03-27 22:33:15 +0000

    clearing as i changed a couple things and forgot to add them here

--- src/main/java/com/onrappture/whoslow/GlowState.java
+++
@@ -1,8 +0,0 @@
-package com.onrappture.whoslow;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.Map;
-import java.util.UUID;
-public class GlowState {
-    public static final Map<UUID, Long> ATTACKED_PLAYERS = new ConcurrentHashMap<>();
-}
-// #made by mathew love u all <3
